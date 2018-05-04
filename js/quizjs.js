aedbaccc70f645b707d14698afb900b661e5683d
Aeuke Peer Conradi <aeuke-peer@t-online.de>
2018-05-04 11:12:16 +0200

    Überprüfung der Antworten des Quiz

--- js/quizjs.js
+++ js/quizjs.js
@@ -18,6 +18,8 @@
     ]
 */
 Element.prototype.quizJS = function (params) {
+
+    var allefragen = [];
     var element = $(this);
     var karte = document.createElement("div");
     karte.classList.add("card");
@@ -36,15 +38,102 @@ Element.prototype.quizJS = function (params) {
     var jfile = $.getJSON(params.fragen, function (json) {
         console.log(json.Namen);
         //Durchgehen durch alle Fragen
+        allefragen = json.Fragen;
         jQuery.each(json.Fragen, function (i, val) {
+
             var frage = document.createElement("div");
+            frage.id = "question_" + i;
             frage.classList.add("form-group");
             var frage_text = document.createElement("label");
             frage_text.innerHTML = val.Frage;
             frage.append(frage_text);
             kartenkoerper.append(frage);
-            console.log(i + ' - ' + val.Frage);
+            console.log(i + ' - ' + val.type);
+            if (val.type == "auswahl") {
+                var auswahl = document.createElement("select");
+                auswahl.id = "answer_" + i;
+                auswahl.classList.add("form-control");
+                auswahl.classList.add("antwort");
+                jQuery.each(val.Antworten, function (i, antwort) {
+                    var auswahlelement = document.createElement("option");
+
+                    auswahlelement.innerText = antwort.Antwort;
+
+                    auswahl.append(auswahlelement);
+                });
+                frage.append(auswahl);
+            } else if (val.type == "eingabe") {
+                var eingabe = document.createElement("input");
+                eingabe.id = "answer_" + i;
+                eingabe.classList.add("form-control");
+                eingabe.classList.add("antwort");
+                frage.append(eingabe);
+            } else if (val.type == "schaetzung") {
+                var eingabe = document.createElement("input");
+                eingabe.id = "answer_" + i;
+                eingabe.classList.add("form-control");
+                eingabe.classList.add("antwort");
+                frage.append(eingabe);
+            }
         });
     });
+
+    var kartenfuss = document.createElement("div");
+    kartenfuss.classList.add("card-footer");
+
+    var submitbutton = document.createElement("button");
+    submitbutton.id = "submitquiz";
+    submitbutton.classList.add("btn");
+    submitbutton.classList.add("btn-success");
+    submitbutton.innerText = "Fertig";
+    kartenfuss.append(submitbutton);
+    karte.append(kartenfuss);
+
+    $('#submitquiz').click(function () {
+        console.log("Quiz abgegeben");
+        $('.antwort').each(function (index) {
+            console.log($(this).val());
+            var fr = allefragen[index];
+            console.log(fr);
+            if (fr.type == "auswahl") {
+                var richtig =
+                    fr.Antworten.filter(function (data) {
+                        return data.richtig == true
+                    });
+                console.log(richtig[0].Antwort);
+                if ($(this).val() == richtig[0].Antwort) {
+                    console.log("== richtige Antwort == ");
+                } else {
+                    $(this).addClass("is-invalid");
+                    var wrongcallback = document.createElement("div")
+                    wrongcallback.classList.add("invalid-feedback");
+                    wrongcallback.innerText = "Das war falsch";
+                    $(this).parent().append(wrongcallback);
+                }
+            } else if (fr.type == "eingabe") {
+                var richtig = fr.Antwort;
+                if ($(this).val() == richtig) {
+                    console.log("== richtige Antwort == ");
+                } else {
+                    $(this).addClass("is-invalid");
+                    var wrongcallback = document.createElement("div")
+                    wrongcallback.classList.add("invalid-feedback");
+                    wrongcallback.innerText = "Das war falsch";
+                    $(this).parent().append(wrongcallback);
+                }
+            } else if (fr.type == "schaetzung") {
+                var richtig = fr.Antwort;
+                if ($(this).val() >= richtig - fr.Toleranz && $(this).val() <= richtig + fr.Toleranz) {
+                    console.log("== richtige Antwort == ");
+                } else {
+                    $(this).addClass("is-invalid");
+                    var wrongcallback = document.createElement("div")
+                    wrongcallback.classList.add("invalid-feedback");
+                    wrongcallback.innerText = "Das war falsch";
+                    $(this).parent().append(wrongcallback);
+                }
+            }
+        });
+    })
 
 }
